87480f0bed5537625c34837fd3d14227e5b4ee7e
SaraElsaggan <sara.mahmoud.sm600@gmail.com>
2023-10-23 15:20:13 +0300

    commit2

--- main.py
+++ main.py
@@ -16,10 +16,20 @@ class MyMainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowTitle("Your Application Title")
         
-        self.siganls_g1 = []
+        self.time = np.linspace(0, 10, 1000)
         
+        self.siganls_g1 = []
+        self.signals_components = []
+        
+        self.ui.horizontalSlider.setMinimum(0)
+        self.ui.horizontalSlider.setMaximum(100)
+        self.ui.horizontalSlider.valueChanged.connect(self.add_noise)
         
         self.ui.pushButton.clicked.connect(self.load_signal)
+        self.ui.pushButton_2.clicked.connect(self.create_signal_component)
+        self.ui.pushButton_3.clicked.connect(self.remove_component)
+        self.ui.pushButton_4.clicked.connect(self.add_noise)
+        
         
         
     def load_signal(self):
@@ -28,14 +38,103 @@ class MyMainWindow(QMainWindow):
         x = data[:, 0].tolist()
         y = data[:, 1].tolist()
         file_name = os.path.basename(file_path[:-4])
+        self.ui.graphicsView_2.clear()        
+        self.ui.graphicsView_2.plotItem.plot(x , y , pen="#ffffff")
+        self.ui.graphicsView_2.plotItem.vb.setLimits(xMin=0 , xMax=max(x), yMin=min(y) , yMax=max(y))
+    
+    def create_signal_component(self):
+        amplitude, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your amplitude:') # this is just for now 
+        phase, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your phase:')
+        frequency, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your frequency:')
         
-        self.ui.graphicsView.plotItem.plot(x , y , pen="#ffffff")
+        amplitude = float(amplitude)
+        phase = float(phase)
+        frequency = float(frequency)
+        
+        signal_component = {
+            "amplitude" : amplitude, 
+            "phase" : phase,
+            "frequency" : frequency,
+        }
+        
+        # signal_component = amplitude * np.sin(2 * np.pi * frequency  * self.time + phase)
+        self.signals_components.append(signal_component)
+        # self.signals_components.append(signal_component)
+        
+        self.update_component_list()        
+        self.plot_composed_signal()
+        # self.ui.graphicsView_2.plot(self.time, signal, pen='b')
+        
+    def plot_composed_signal(self):
+        self.ui.graphicsView_2.clear()
+        signal = self.signals_components[0]['amplitude'] * np.sin(2 * np.pi * self.signals_components[0]['frequency']  * self.time + self.signals_components[0]['phase'])
+        # signal = np.zeros(len(self.time))
+
+        # signal =  np.sin(0)
+        for component in self.signals_components[1:]:
+            add_comp = component['amplitude'] * np.sin(2 * np.pi * component['frequency']  * self.time + component['phase'])
+            signal += add_comp
+        self.mixed_signal = signal
+        plotted_signal = self.ui.graphicsView_2.plot(signal)
+        
+        min_x =  min(plotted_signal.getData()[0])
+        max_x =  max(plotted_signal.getData()[0])
+
+
+        min_y =  min(plotted_signal.getData()[1])
+        max_y =  max(plotted_signal.getData()[1])
+
+        self.ui.graphicsView_2.plotItem.vb.setLimits(xMin=min_x , xMax=max_x, yMin=min_y , yMax=max_y)
         
     
-    def update_signals_list(self):
+    # def plot_composed(self):
+    def update_component_list(self):
         self.ui.listWidget.clear()
-        for signal in self.siganls_g1:
-            self.ui.listWidget.addItem(signal["name"])
+        for compnent in self.signals_components:
+            amplitude = compnent["amplitude"]
+            frequency = compnent["frequency"]
+            phase = compnent["phase"]
+            self.ui.listWidget.addItem(f"signal component : {len(self.signals_components)}  amplitude : {amplitude} , frequency : {frequency} , phase : {phase}")
+            # print(self.ui.listWidget.item(0).text())
+            
+            
+    
+    def remove_component(self):
+        delete_component_indx = self.ui.listWidget.currentRow()   
+        self.signals_components.pop(delete_component_indx)
+        self.plot_composed_signal()
+        self.update_component_list()
+        # print(self.signals_components[delete_component_indx]["phase"])
+    
+    def add_noise(self ):
+        snr_level , ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your amplitude:') 
+        snr_level = float(snr_level)   
+        # self.ui.graphicsView_2.clear()
+        signal_with_noise = self.mixed_signal
+        # snr_level = self.ui.horizontalSlider.value()    
+        noise = np.random.normal(0, snr_level, len(self.time))
+        signal_with_noise += noise
+        self.ui.graphicsView_2.clear()
+        self.ui.graphicsView_2.plot(signal_with_noise )
+        
+        
+    # def rempove_compnent (self):
+    #     component = self.ui.listWidget.currentItem()
+    #     for component in self.signals_components:
+    #         if 
+        
+        
+        
+        
+        
+            
+
+        
+    
+    # def update_signals_list(self):
+    #     self.ui.listWidget.clear()
+    #     for signal in self.siganls_g1:
+    #         self.ui.listWidget.addItem(signal["name"])
 
 def main():
     app = QApplication(sys.argv)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+  min(self.ui.graphicsView_2.plotItem.getData()[0])
+        # max_x =  max(self.ui.graphicsView_2.plotItem.getData()[0])
+
+
+        # min_y =  min(self.ui.graphicsView_2.plotItem.getData()[1])
+        # max_y =  max(self.ui.graphicsView_2.plotItem.getData()[1])
+
+        # self.ui.graphicsView.
